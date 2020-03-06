3d99a43c6d62bfa461c1e0d5723c675cd2d2bc28
Peter Jang <peter.x.jang@gmail.com>
2020-03-05 21:23:44 -0600

    Add RESTful products update action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -20,4 +20,15 @@ class Api::ProductsController < ApplicationController
     @product = Product.find_by(id: product_id)
     render "show.json.jb"
   end
+
+  def update
+    product_id = params["id"]
+    @product = Product.find_by(id: product_id)
+    @product.name = params["name"] || @product.name
+    @product.price = params["price"] || @product.price
+    @product.image_url = params["image_url"] || @product.image_url
+    @product.description = params["description"] || @product.description
+    @product.save
+    render "show.json.jb"
+  end
 end
