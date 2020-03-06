bf55494be6c293e1899556853277449ddbfaf933
Peter Jang <peter.x.jang@gmail.com>
2020-03-05 21:22:03 -0600

    Add RESTful products create action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,6 +4,17 @@ class Api::ProductsController < ApplicationController
     render "index.json.jb"
   end
 
+  def create
+    @product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+    @product.save
+    render "show.json.jb"
+  end
+
   def show
     product_id = params["id"]
     @product = Product.find_by(id: product_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
+    post "/products" => "products#create"
     get "/products/:id" => "products#show"
   end
 end
