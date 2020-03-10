16b934eefcc7740ec9b49997411827c74f85240b
Peter Jang <peter.x.jang@gmail.com>
2020-03-09 20:59:22 -0500

    Add product validations with happy/sad paths for create/update actions

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+
   def is_discounted?
     price < 10
   end
