d3bb9377fa4999d833ab55e9dc99b2c2c87b5355
Peter Jang <peter.x.jang@gmail.com>
2020-03-12 20:01:48 -0500

    Fix create and update to reflect new database changes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -23,8 +23,8 @@ class Api::ProductsController < ApplicationController
     @product = Product.new(
       name: params["name"],
       price: params["price"],
-      image_url: params["image_url"],
       description: params["description"],
+      supplier_id: params["supplier_id"],
     )
     if @product.save
       render "show.json.jb"
@@ -44,7 +44,6 @@ class Api::ProductsController < ApplicationController
     @product = Product.find_by(id: product_id)
     @product.name = params["name"] || @product.name
     @product.price = params["price"] || @product.price
-    @product.image_url = params["image_url"] || @product.image_url
     @product.description = params["description"] || @product.description
     if @product.save
       render "show.json.jb"
