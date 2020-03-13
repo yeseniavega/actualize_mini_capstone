bc116ca61040db4e2525c881507009edb92e3c5c
Peter Jang <peter.x.jang@gmail.com>
2020-03-12 20:13:10 -0500

    Bring back ability to save associated image on product create action

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -27,6 +27,7 @@ class Api::ProductsController < ApplicationController
       supplier_id: params["supplier_id"],
     )
     if @product.save
+      Image.create(url: params["image_url"], product_id: @product.id)
       render "show.json.jb"
     else
       render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
