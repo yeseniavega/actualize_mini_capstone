71b0d2621592d4146fd2b4b6605ac15033838cb3
Peter Jang <peter.x.jang@gmail.com>
2020-03-23 09:45:00 -0500

    Refactor order calculations to model method to clean up controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -7,31 +7,13 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    carted_products = current_user.shopping_cart
-
-    calculated_subtotal = 0
-    calculated_tax = 0
-    calculated_total = 0
-    carted_products.each do |carted_product|
-      calculated_subtotal += carted_product.quantity * carted_product.product.price
-      calculated_tax += carted_product.quantity * carted_product.product.tax
-    end
-    calculated_total = calculated_subtotal + calculated_tax
-
     @order = Order.new(
       user_id: current_user.id,
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
     )
     if @order.save
+      carted_products = current_user.shopping_cart
       carted_products.update_all(status: "purchased", order_id: @order.id)
-      # THE LONG WAY
-      # carted_products.each do |carted_product|
-      #   carted_product.status = "purchased"
-      #   carted_product.order_id = @order.id
-      #   carted_product.save
-      # end
+      @order.update_totals
       render "show.json.jb"
     else
       render json: { errors: @order.errors.full_messages }, status: :unprocessable_entity
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,16 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :carted_products
   has_many :products, through: :carted_products
+
+  def update_totals
+    calculated_subtotal = 0
+    calculated_tax = 0
+    calculated_total = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+      calculated_tax += carted_product.quantity * carted_product.product.tax
+    end
+    calculated_total = calculated_subtotal + calculated_tax
+    update(subtotal: calculated_subtotal, tax: calculated_tax, total: calculated_total)
+  end
 end
